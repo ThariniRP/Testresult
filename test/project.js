f0a1a696428b7914db65b841db31550fb8477ad8
aj <akshayajanani12@gmail.com>
2018-06-06 17:56:38 +0530

    file is added

---
+++ test/project.js
@@ -0,0 +1,8 @@
+
+describe("Some module", function() {
+    if(false) {
+        it ("should NOT run this test case", function() { });
+    }
+
+    it("should run this test case", function() { });
+});
