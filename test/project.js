db22219f07e04ecbb24eaf6836af233b4c4b5745
akshayajanani12 <36192918+akshayajanani12@users.noreply.github.com>
2018-06-07 09:05:00 +0530

    Update project.js

--- test/project.js
+++ test/project.js
@@ -5,4 +5,23 @@ describe("Some module", function() {
     }
 
     it("should run this test case", function() { });
+    
+    describe("Some module", function() {
+    if(false) {
+        it ("should NOT run this test case", function() { 
+        });
+    }
+
+    it("should run this test case", function() { 
+    });
+});
+    describe("Some module", function() {
+    if(false) {
+        it ("should NOT run this test case", function() { 
+        });
+    }
+
+    it("should run this test case", function() {
+    });
+});
 });
